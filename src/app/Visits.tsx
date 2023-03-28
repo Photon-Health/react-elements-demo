2855a109d189018eb6225f85c5676741c89e4c9f
Paul Christophe <paul.christophe6@gmail.com>
2023-03-28 14:33:50 -0400

    switch to neutron

--- src/app/Visits.tsx
+++ src/app/Visits.tsx
@@ -12,9 +12,9 @@ function Visits() {
       <photon-client
         id="7N9QZujlNJHL8EIPqXpu1wq8OuXqoxKb"
         org="org_KzSVZBQixLRkqj5d"
-        domain="auth.boson.health"
-        audience="https://api.boson.health"
-        uri="https://api.boson.health/graphql"
+        domain="auth.neutron.health"
+        audience="https://api.neutron.health"
+        uri="https://api.neutron.health/graphql"
         auto-login="true"
         redirect-uri="http://localhost:3000?redirect=%2Fvisits"
       >
