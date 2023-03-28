4a29176a46bfe8851434165683f5f3878375ad32
Paul Christophe <paul.christophe6@gmail.com>
2023-03-28 14:20:20 -0400

    set to neutron

--- src/app/Visits.tsx
+++ src/app/Visits.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { useState } from 'react';
 
 import('@photonhealth/elements');
@@ -11,11 +12,11 @@ function Visits() {
       <photon-client
         id="7N9QZujlNJHL8EIPqXpu1wq8OuXqoxKb"
         org="org_KzSVZBQixLRkqj5d"
-        redirect-uri="http://localhost:3000?redirect=%2Fvisits"
-        domain="auth.boson.health"
-        audience="https://api.boson.health"
-        uri="https://api.boson.health/graphql"
+        domain="auth.neutron.health"
+        audience="https://api.neutron.health"
+        uri="https://api.neutron.health/graphql"
         auto-login="true"
+        redirect-uri="http://localhost:3000?redirect=%2Fvisits"
       >
         {show ? <photon-multirx-form-wrapper hide-templates="false" /> : null}
       </photon-client>
