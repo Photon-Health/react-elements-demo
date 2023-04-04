fd5da708495ad291e43c81ce8fe2762e5f3c8028
Paul Christophe <paul.christophe6@gmail.com>
2023-04-04 15:34:42 -0400

    namespace and neutron

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import * as ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'photon-client': unknown;
+      'photon-multirx-form-wrapper': unknown;
+    }
+  }
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -12,9 +21,9 @@ root.render(
     <photon-client
       id="7N9QZujlNJHL8EIPqXpu1wq8OuXqoxKb"
       org="org_KzSVZBQixLRkqj5d"
-      domain="auth.boson.health"
-      audience="https://api.boson.health"
-      uri="https://api.boson.health/graphql"
+      domain="auth.neutron.health"
+      audience="https://api.neutron.health"
+      uri="https://api.neutron.health/graphql"
       auto-login="true"
       redirect-uri="http://localhost:3000"
       redirect-path="/visits"
