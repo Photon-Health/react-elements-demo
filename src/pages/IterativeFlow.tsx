bf9dc2a33b7f6ab38bcd92e18a90db110a2fca76
Jake Stout <jake@photon.health>
2025-05-21 15:03:14 -0400

    Setup iterative flow tab

--- src/pages/IterativeFlow.tsx
+++ src/pages/IterativeFlow.tsx
@@ -1,12 +1,57 @@
-// @ts-nocheck
+import { useState } from 'react';
+import templateOverridesData, {
+  templateOverridesList,
+  TemplateOverridesProp,
+} from '../data/templateOverridesData';
 
 import('@photonhealth/elements');
 
 export function IterativeFlow() {
+  const [currentTemplateOverrides, setCurrentTemplateOverrides] =
+    useState<TemplateOverridesProp>({});
+
+  const addTemplate1 = () => {
+    const newOverrides = { ...currentTemplateOverrides };
+    newOverrides[templateOverridesList[0].id] =
+      templateOverridesData[templateOverridesList[0].id];
+    setCurrentTemplateOverrides(newOverrides);
+  };
+
+  const addTemplate2 = () => {
+    const newOverrides = { ...currentTemplateOverrides };
+    newOverrides[templateOverridesList[1].id] =
+      templateOverridesData[templateOverridesList[1].id];
+    setCurrentTemplateOverrides(newOverrides);
+  };
+
+  const templateIds = Object.keys(currentTemplateOverrides);
+
   return (
-    <div>
-      <photon-prescribe-workflow />
-    </div>
+    <>
+      <div>
+        <h3>Iterative Wrapper App Controls</h3>
+        <div>
+          <button onClick={addTemplate1}>
+            Add Template 1: {templateOverridesList[0].name}
+          </button>
+        </div>
+        <div>
+          <button onClick={addTemplate2}>
+            Add Template 2: {templateOverridesList[1].name}
+          </button>
+        </div>
+      </div>
+      <div>
+        <h3>Photon Element</h3>
+        {/*@ts-expect-error*/}
+        <photon-prescribe-workflow
+          key={templateIds.join(',')}
+          patient-id={import.meta.env.VITE_PATIENT_ID}
+          template-ids={templateIds}
+          template-overrides={JSON.stringify(currentTemplateOverrides)}
+        />
+      </div>
+    </>
   );
 }
 
